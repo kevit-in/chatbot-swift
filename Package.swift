dc8c8d491e3c31aec894cdcadda177456f4e1908
Arjav Dave <arjav@royalecheese.com>
2025-01-25 20:23:56 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
                 .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.0.0"),
                 .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.6")
             ],
-            url: "https://github.com/kevit-in/ios-chatbot-framework/releases/download/v1.0.0/ChatBot.xcframework.zip"
+            url: "https://github.com/kevit-in/ios-chatbot-framework/releases/download/1.0.0/ChatBot.xcframework.zip"
         )
     ]
 )
