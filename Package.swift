e2a894251884ecb86129c6a9843ca4db4825bb84
Arjav Dave <arjav@royalecheese.com>
2025-01-25 20:20:35 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,10 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChatBot",
+            dependencies: [
+                .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.0.0"),
+                .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.6")
+            ],
             url: "https://github.com/kevit-in/ios-chatbot-framework/releases/download/v1.0.0/ChatBot.xcframework.zip"
         )
     ]
