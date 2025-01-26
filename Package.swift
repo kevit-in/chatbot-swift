e237dd721d9d68100f2c38758aa71096fb1522e6
Arjav Dave <arjav.dave@kevit.io>
2025-01-26 12:17:42 +0530

    First Push

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ChatBot",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ChatBotSwift",
+            targets: ["ChatBotSwift"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "ChatBotSwift",
+            dependencies: [
+                .target(name: "ChatBot"), // Link the binary target
+                "Kingfisher", // Add Kingfisher
+                "Starscream", // Add Starscream
+            ],
+            path: "./Sources"
+        ),
+        .binaryTarget(
+            name: "ChatBot",
+            url: "https://github.com/kevit-in/chatbot-swift/releases/download/1.0.0/ChatBot.xcframework.zip",
+            checksum: "cd17bf1e2c5c8893686c1e25d5c7d62d25caba01b414324039c2c2b309002d42"
+        )
+    ]
+)
---
+++ Sources/dummy.m
@@ -0,0 +1,8 @@
+//
+//  dummy.m
+//  
+//
+//  Created by Arjav Dave on 26/01/25.
+//
+
+#import <Foundation/Foundation.h>
